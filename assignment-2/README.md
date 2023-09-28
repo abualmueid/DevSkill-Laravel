b007971ac29377f6b897a5ef584050356ca0c004
abualmueid <61595430+abualmueid@users.noreply.github.com>
2023-09-28 23:14:33 +0600

    added file

---
+++ assignment-2/README.md
@@ -0,0 +1,122 @@
+# Student Information
+
+Student 1: Name - "John", Age - 20, Favorite Subjects - ["Math", "Science"]  
+Student 2: Name - "Alice", Age - 18, Favorite Subjects - ["History", "English"]  
+Student 3: Name - "Bob", Age - 19, Favorite Subjects - ["Art", "Music"]
+
+## Question 1
+
+Write a PHP function to calculate the average age of the students.
+
+```
+function averageAge($students)
+{
+    $sum = 0;
+    foreach($students as $student)
+    {
+        $sum += $student->getAge();
+    }
+
+    return $sum / count($students);
+}
+```
+
+### Result
+
+The average age of the students is 19
+
+## Question 2
+
+Write a PHP function to find and return the most favorite subject.
+
+### Step-1
+
+Create an array to store how many times each subject is mentioned
+
+```
+$subjectCounts = [];
+    foreach($students as $student)
+    {
+        $favoriteSubjects = $student->getFavoriteSubjects();
+        foreach($favoriteSubjects as $subject)
+        {
+            if(!isset($subjectCounts[$subject]))
+                $subjectCounts[$subject] = 0;
+            if(isset($subjectCounts[$subject]))
+                $subjectCounts[$subject]++;
+        }
+    }
+```
+
+### Step-2
+
+Find the subject with the most mentions
+
+```
+$maxCount = 0;
+$mostFavoriteSubject = null;
+
+foreach($subjectCounts as $subject => $count)
+{
+    if($count > $maxCount)
+    {
+        $maxCount = $count;
+        $mostFavoriteSubject = $subject;
+    }
+}
+```
+
+### Result
+
+The most favorite subject is English
+
+## Question 3
+
+Encode the student information array into JSON format & print Json.
+
+```
+foreach($students as $student)
+{
+    $encodedStudent = json_encode($student->toArray());
+    echo $encodedStudent;
+}
+```
+
+### Result
+
+JSON Format: 
+
+{"name":"John","age":20,"favoriteSubjects":["Math","Science","English"]}{"name":"Alice","age":18,"favoriteSubjects":["History","English"]}{"name":"Bob","age":19,"favoriteSubjects":["Art","Music"]}
+
+## Question 4
+
+Display the information of each student, including their name, age, and favorite subjects. 
+
+```
+$i = 1;
+foreach($students as $student)
+{
+    echo "\n\nStudent-$i:\n";
+    $student->displayStudent();
+    $i++;
+}
+```
+
+### Result
+
+Student's Information:  
+
+Student-1:  
+Name: John  
+Age: 20  
+Favorite Subjects: Math, Science, English  
+
+Student-2: <br>
+Name: Alice <br>
+Age: 18 <br>
+Favorite Subjects: History, English <br>
+
+Student-3: <br>
+Name: Bob <br>
+Age: 19 <br>
+Favorite Subjects: Art, Music
---
+++ assignment-2/index.php
@@ -0,0 +1,129 @@
+<?php
+
+class Student
+{
+    private string $name;
+    private int $age;
+    private array $favoriteSubjects;
+
+    public function __construct(string $name, int $age, array $favoriteSubjects)
+    {
+        $this->name = $name;
+        $this->age = $age;
+        $this->favoriteSubjects = $favoriteSubjects;
+    }
+
+    public function toArray() : array
+    {
+        return 
+        [
+            "name" => $this->name,
+            "age" => $this->age,
+            "favoriteSubjects" => $this->favoriteSubjects
+        ];
+    }
+
+    public function getName() : string
+    {
+        return $this->name;
+    }
+
+    public function getAge() : float
+    {
+        return $this->age;
+    }
+
+    public function getFavoriteSubjects() : array
+    {
+        return $this->favoriteSubjects;
+    }
+
+    public function displayStudent()
+    {
+        echo "Name: $this->name\n";
+        echo "Age: $this->age\n";
+        echo "Favorite Subjects: ";
+
+        $favoriteSubjectsString = "";
+        foreach($this->favoriteSubjects as $favoriteSubject)
+        {
+            $favoriteSubjectsString .= $favoriteSubject . ", ";
+        }
+        echo rtrim($favoriteSubjectsString, ", ");
+    }
+}
+
+$s1 = new Student("John", 20, ["Math", "Science", "English"]);
+$s2 = new Student("Alice", 18, ["History", "English"]);
+$s3 = new Student("Bob", 19, ["Art", "Music"]);
+
+$students = [$s1, $s2, $s3];
+echo("\nThe average age of the students is " . averageAge($students) . "\n\n");
+
+function averageAge($students)
+{
+    $sum = 0;
+    foreach($students as $student)
+    {
+        $sum += $student->getAge();
+    }
+
+    return $sum / count($students);
+}
+
+echo("The most favorite subject is " . findTheMostFavoriteSubject($students));
+
+function findTheMostFavoriteSubject($students)
+{
+    // Create an array to store how many times each subject is mentioned
+
+    $subjectCounts = [];
+    foreach($students as $student)
+    {
+        $favoriteSubjects = $student->getFavoriteSubjects();
+        foreach($favoriteSubjects as $subject)
+        {
+            if(!isset($subjectCounts[$subject]))
+                $subjectCounts[$subject] = 0;
+            if(isset($subjectCounts[$subject]))
+                $subjectCounts[$subject]++;
+        }
+    }
+
+    // Find the subject with the most mentions
+
+    $maxCount = 0;
+    $mostFavoriteSubject = null;
+
+    foreach($subjectCounts as $subject => $count)
+    {
+        if($count > $maxCount)
+        {
+            $maxCount = $count;
+            $mostFavoriteSubject = $subject;
+        }
+    }
+
+    return $mostFavoriteSubject;
+}
+
+// JSON encoding
+echo("\n\nJSON Format: \n\n");
+foreach($students as $student)
+{
+    $encodedStudent = json_encode($student->toArray());
+    echo $encodedStudent;
+}
+
+// JSON decoding
+$decodedStudent = json_decode($encodedStudent);
+// print_r($decodedStudent);
+
+echo("\n\nStudent's Information:");
+$i = 1;
+foreach($students as $student)
+{
+    echo "\n\nStudent-$i:\n";
+    $student->displayStudent();
+    $i++;
+}
