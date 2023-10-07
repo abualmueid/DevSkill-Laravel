fcc292ce735eb4d6464528b85f4de945430cb6a5
abualmueid <61595430+abualmueid@users.noreply.github.com>
2023-10-07 18:33:46 +0600

    modified file

--- oop-assignment-1/index.php
+++ oop-assignment-1/index.php
@@ -1,2 +1,55 @@
 <?php 
 
+class Student
+{
+    private int $id;
+    private string $name;
+    private string $major;
+
+    private array $students;
+
+    public function addStudent(array $students) : void
+    {
+        foreach($students as $student)
+        {
+            $id = $student['id'] ?? uniqid();
+            $this->students[$id] = $student;
+        }
+    }
+
+    public function getStudents() : array 
+    {
+        return $this->students;
+    }
+
+    public function getStudent($id) : array
+    {
+        return $this->students[$id] ?? []; 
+        // return isset($this->students[$id]) ? $this->students[$id] : []; 
+        // You don't need foreach loop here. Because it's already there inside isset() method.
+    }
+}
+
+$students = [
+    [
+        "id" => 1520730,
+        "name" => "Mueid",
+        "major" => "CS"
+    ],
+    [
+        "id" => 1621321,
+        "name" => "Rasel",
+        "major" => "EEE"
+    ],
+    [
+        "id" => 1756669,
+        "name" => "Mustafiz",
+        "major" => "ETE"
+    ]
+];
+
+$student = new Student();
+$student->addStudent($students);
+echo json_encode($student->getStudents());
+//echo json_encode($student->getStudent(1756669));
+
