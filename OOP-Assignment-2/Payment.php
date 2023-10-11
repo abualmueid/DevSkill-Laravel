2ac6c2fdaac8e8490026134d3ecfb3816ea63c5f
abualmueid <61595430+abualmueid@users.noreply.github.com>
2023-10-11 20:26:49 +0600

    modified file

--- OOP-Assignment-2/Payment.php
+++ OOP-Assignment-2/Payment.php
@@ -4,17 +4,27 @@ trait Payment
 {
     private array $payments;
 
-    public function addPayment(int $bookId, int|float $amount) : bool
+    public function addPayment(int $bookId, int|float $amount, object $book) : bool
     {
-        if($bookId)
-        $this->payments[$bookId] = $amount;
+        if($book->getBook($bookId)['price'] !== $amount)
+        {
+            echo "Payment Unsuccessful!";
 
-        return true;
+            return false;
+        }
+        
+        if($book->getBook($bookId)['price'] === $amount)
+        {
+            $this->payments[$bookId] = $amount;
+            echo "Payment Successful!";
+
+            return true;
+        }
     }
 
     public function getPayment(int $bookId) : int|float
     {
-        return $this->payments[$bookId];
+        return $this->payments[$bookId] ?? 0;
     }
 
 }
--- OOP-Assignment-2/index.php
+++ OOP-Assignment-2/index.php
@@ -1,46 +1,80 @@
 <?php
 
+include('Customer.php');
+include('Payment.php');
+
 class Book
 {
+    use Customer, Payment;
+
     private array $books;
 
-    public function addBook(array $book) : void
+    public function addBook(array $book) : bool
     {
         $uId = $book['uId'];
-        $name = $book['name'];
-        $price = $book['price'];
-
         $this->books[$uId] = $book;
 
-        //return true;
+        return true;
     }
 
     public function getBooks() : array
     {
         return $this->books ?? [];
     }
+
+    public function getBook($bookId) : array
+    {
+        return $this->books[$bookId] ?? [];
+    }
 }
 
 $book = new Book();
+
 $bookInfo = [
-    "uId" => "1001",
+    "uId" => 1001,
     "name" => "Bela Furabar Age",
-    "price" => "150"
+    "price" => 150
 ];
 $book->addBook($bookInfo);
 
 $bookInfo = [
-    "uId" => "1002",
+    "uId" => 1002,
     "name" => "Islami Akida",
-    "price" => "320"
+    "price" => 320
 ];
 $book->addBook($bookInfo);
 
 $bookInfo = [
-    "uId" => "1003",
+    "uId" => 1003,
     "name" => "Hadiser name jaliyati",
-    "price" => "350"
+    "price" => 350
 ];
 $book->addBook($bookInfo);
 
 echo json_encode($book->getBooks());
+
+$book->addCustomer(1001, [
+    "name" => "Abul Kashem",
+    "mobileNo" => "01745369875"
+]);
+
+$book->addCustomer(1002, [
+    "name" => "Babul Hashem",
+    "mobileNo" => "01745364889"
+]);
+
+$book->addCustomer(1003, [
+    "name" => "Abdul Kader",
+    "mobileNo" => "0174536658" // Invalid Mobile No
+]);
+
+echo json_encode($book->getCustomers());
+echo json_encode($book->getCustomer(1002));
+
+$book->addPayment(1001, 150, $book);
+$book->addPayment(1002, 300, $book); // less amount than book price, payment unsuccessful
+$book->addPayment(1003, 350, $book);
+
+echo json_encode($book->getPayment(1001));
+echo json_encode($book->getPayment(1002));
+echo json_encode($book->getPayment(1003));
