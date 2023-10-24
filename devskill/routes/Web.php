de20feb9882a4aa472ae9f2ce885de0275e777db
abualmueid <61595430+abualmueid@users.noreply.github.com>
2023-10-24 22:57:43 +0600

    modified file

--- devskill/routes/Web.php
+++ devskill/routes/Web.php
@@ -4,3 +4,6 @@ use App\Controllers\AppController;
 use DevSkill\Supports\Route;
 
 Route::get('/', [AppController::class, 'index']);
+
+$route = new Route();
+$route->get('/', [AppController::class, 'index']);
--- devskill/system/Supports/Route.php
+++ devskill/system/Supports/Route.php
@@ -4,10 +4,19 @@ namespace DevSkill\Supports;
 
 class Route
 {
-    public static function __callStatic($method, $args)
+    public static function __callStatic($method, $args): void
     {
         $callback = $args[1];
         $controller = new $callback[0]();
         $controller->{$callback[1]}();
+    }
+
+    public function __call($method, $args): void
+    {
+        $callback = $args[1];
+        $controller = new $callback[0]();
+        $controller->{$callback[1]}();
+
+        echo $method;
     }
 }
