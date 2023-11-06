ee3570bc1840056c3dd9ce4de88385a6f6f25d55
abualmueid <61595430+abualmueid@users.noreply.github.com>
2023-11-07 01:12:51 +0600

    moved to devskill folder

--- middleware/devskill/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-include('vendor/autoload.php');
-
-// $application = new \DevSkill\Application(__DIR__);
-$application = \DevSkill\Application::instance(__DIR__);
-
-// echo $application->rootPath();
-echo $application->boot();
