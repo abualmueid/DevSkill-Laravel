f33a835e31db7fd3dcd3a504061d185338c783db
abualmueid <61595430+abualmueid@users.noreply.github.com>
2023-12-07 01:56:43 +0600

    modified file

--- example-app/resources/views/welcome.blade.php
+++ example-app/resources/views/welcome.blade.php
@@ -136,5 +136,13 @@
                 </div>
             </div>
         </div>
+
+        <h1>{{!! $user->name !!}}</h1>
+
+        @if($user)
+            <h1>{{$user->name}}</h1>
+        @endif
+
+
     </body>
 </html>
